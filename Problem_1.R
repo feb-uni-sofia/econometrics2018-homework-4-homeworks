894b70c14ed892bd982537c57777a55423cc4b0d
Lubomir Oresharov <l.oresharov@gmail.com>
2018-06-10 18:11:57 +0300

    dddd

--- Problem_1.R
+++ Problem_1.R
@@ -8,20 +8,53 @@ str(houseWork)
 
 ## a)
 
+summary(houseWork)
+
 ## b)
+
+muf <- mean(subset(houseWork$hours, houseWork$sex == 'f'))
+mum <- mean(subset(houseWork$hours, houseWork$sex == 'm'))
 
 ## c)
 
+houseWork <- within(houseWork, {
+  female <- ifelse(sex == 'f', 'TRUE', 'FALSE')
+  male <- ifelse(sex == 'm', 'TRUE', 'FALSE')
+})
+
+houseWork
+
 ## d)
+
+fit <- lm(hours ~ female, data = houseWork)
+summary(fit)
 
 ## e)
 
+
+##Intercept is an estimator for b0.
+##femaleTRUE is an estimator for b1.
+##Both are regression coefficients.
+
 ## f)
+
+##The null hypothesis states, that the average house-work hours for women is
+##less or equal to the house-work for men. The alternative states, that the house-work for women
+##is greater than the house-work for men.
 
 ## g)
 
+T <- (-14.4555 - 32.8138) / 0.3186
+
+pt(-T, df = 2)
+
 ## h)
+
+##The P-value is greater than 0.05, so we do not reject the null hypothesis.
 
 ## i)
 
 ## j)
+
+fit1 <- lm(hours ~ female + male, data = houseWork)
+summary(fit1)
--- Problem_2.R
+++ Problem_2.R
@@ -4,10 +4,41 @@ str(crime)
 
 ## a)
 
+fit <- lm(C ~ HS, data = crime)
+summary(fit)
+
+##Intercept is an estimator for b0
+##HS is an estimator for b1
+##Both are called regression coefficients
+
 ## b)
+
+pairs( ~ C + U + I + HS, data = crime)
 
 ## c)
 
+fit1 <- lm(C ~ HS + U, data = crime)
+summary(fit1)
+
+## intercept is estimator for b0
+## HS is an estimator for b1
+## U is an estimator for b2
+## With U in the model the value of the Estimated coefficient
+## becomes less than without U in the model
+
 ## d)
 
+pairs( ~ C + HS, data = crime)
+
+##The politician's claims that the lower education spending will reduce the crime rates are wrong.
+##We can see from the scatterplot matrix, that the lower HS bring more crimes.
+
 ## e)
+
+fit3 <- lm(C ~ HS + U + I, data = crime)
+summary(fit3)
+
+
+
+
+
